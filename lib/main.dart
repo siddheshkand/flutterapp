e90bedba98a52dc4e82cccfdc999ecfbcac93ad3
siddheshkand <siddkand@gmail.com>
2018-10-11 23:57:44 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+// Need to import helper library
+import 'package:flutter/material.dart';
+
+// From flutter to get content on screen
+
+// Define main function
+
+void main() {
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(),
+    ),
+  );
+
+// Take widget and get it on screen
+  runApp(app);
+}
