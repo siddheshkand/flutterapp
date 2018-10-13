186652e9b100d07d19fe67812d9560bf4f4a0e30
siddheshkand <siddkand@gmail.com>
2018-10-13 12:39:41 +0530

    App logic separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
-// Need to import helper library
 import 'package:flutter/material.dart';
-
-// From flutter to get content on screen
-
-// Define main function
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(),
-    ),
-  );
-
-// Take widget and get it on screen
-  runApp(app);
+  runApp(App());
 }
